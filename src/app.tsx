099a689ec49b5e9a418cfb3c50cceac54eeddc37
harbassan <harbassan@hotmail.com>
2023-06-04 17:55:18 +1200

    feat: notify users of new release

--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,7 @@ import TracksPage from "./pages/top_tracks";
 import GenresPage from "./pages/top_genres";
 import LibraryPage from "./pages/library";
 import "./css/app.scss";
+import { STATS_VERSION, LATEST_RELEASE } from "./constants";
 
 const pages: Record<string, JSX.Element> = {
     ["Artists"]: <ArtistsPage />,
@@ -13,13 +14,33 @@ const pages: Record<string, JSX.Element> = {
     ["Library"]: <LibraryPage />,
 };
 
+const checkForUpdates = (setNewUpdate: (a: boolean) => void) => {
+    fetch(LATEST_RELEASE)
+        .then(res => res.json())
+        .then(
+            result => {
+                try {
+                    setNewUpdate(result[0].name.slice(1) === STATS_VERSION);
+                } catch (err) {
+                    console.log(err);
+                }
+            },
+            error => {
+                console.log("Failed to check for updates", error);
+            }
+        );
+};
+
 const App = () => {
     const [navBar, activeLink, setActiveLink] = useNavigationBar(["Artists", "Tracks", "Genres", "Library"]);
+    const [newUpdate, setNewUpdate] = React.useState(false);
 
     console.log("app render");
+    console.log(newUpdate);
 
     React.useEffect(() => {
         setActiveLink(Spicetify.LocalStorage.get("stats:active-link") || "Artists");
+        checkForUpdates(setNewUpdate);
     }, []);
 
     React.useEffect(() => {
@@ -28,6 +49,11 @@ const App = () => {
 
     return (
         <>
+            {newUpdate && (
+                <div className="new-update">
+                    New app update available! Visit <a href="https://github.com/harbassan/spicetify-stats/releases">harbassan/spicetify-stats</a> to install.
+                </div>
+            )}
             {navBar}
             {pages[activeLink]}
         </>
---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+import { version } from "../package.json";
+
+export const STATS_VERSION = version;
+
+export const LATEST_RELEASE = "https://api.github.com/repos/harbassan/spicetify-stats/releases";
