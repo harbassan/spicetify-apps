df82ee54b7353ff4477ae11ac8b99397a625a257
Grigory <techuila.sunset@gmail.com>
2023-11-17 04:38:53 +0500

    chore(components/stat_card): remove draggable class (#42)

--- src/components/stat_card.tsx
+++ src/components/stat_card.tsx
@@ -4,15 +4,13 @@ const StatCard = (props: any) => {
     return (
         <>
             <div className="main-card-card">
-                <div draggable="true" className="main-card-draggable">
-                    <div className="stats-cardValue">{props.value}</div>
-                    <div>
-                        <div
-                            className="TypeElement-balladBold-textBase-4px-type main-cardHeader-text stats-cardText"
-                            data-encore-id="type"
-                        >
-                            {props.stat}
-                        </div>
+                <div className="stats-cardValue">{props.value}</div>
+                <div>
+                    <div
+                        className="TypeElement-balladBold-textBase-4px-type main-cardHeader-text stats-cardText"
+                        data-encore-id="type"
+                    >
+                        {props.stat}
                     </div>
                 </div>
             </div>
