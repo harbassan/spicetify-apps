302355b6f6e5415398382210372a52b4d97888ff
harbassan <harbassan@hotmail.com>
2024-08-10 18:52:11 +1200

    fix(stats/converter): cache objects before minification

--- stats/src/utils/converter.ts
+++ stats/src/utils/converter.ts
@@ -49,76 +49,72 @@ export const minifyTrack = (track: Spotify.Track): SpotifyMinifiedTrack => ({
 });
 
 export const convertArtist = async (artist: LastFM.Artist) => {
-	return cacher(async () => {
+	const spotifyArtist = await cacher(async () => {
 		const searchRes = await searchForArtist(artist.name);
 		const spotifyArtists = searchRes.filter(
 			(a) => a.name.localeCompare(artist.name, undefined, { sensitivity: "base" }) === 0,
 		);
-		const spotifyArtist = spotifyArtists.sort((a, b) => b.popularity - a.popularity)[0];
-		if (!spotifyArtist)
-			return {
-				name: artist.name,
-				playcount: Number(artist.playcount),
-				uri: artist.url,
-				type: "lastfm",
-			} as LastFMMinifiedArtist;
-		return {
-			...minifyArtist(spotifyArtist),
-			playcount: Number(artist.playcount),
-			name: artist.name,
-		} as SpotifyMinifiedArtist;
+		return spotifyArtists.sort((a, b) => b.popularity - a.popularity)[0];
 	})({ queryKey: ["searchForArtist", artist.name] });
+	if (!spotifyArtist)
+		return {
+			name: artist.name,
+			playcount: Number(artist.playcount),
+			uri: artist.url,
+			type: "lastfm",
+		} as LastFMMinifiedArtist;
+	return {
+		...minifyArtist(spotifyArtist),
+		playcount: Number(artist.playcount),
+		name: artist.name,
+	} as SpotifyMinifiedArtist;
 };
 
 export const convertAlbum = async (album: LastFM.Album) => {
-	return await cacher(async () => {
+	const spotifyAlbum = await cacher(async () => {
 		const searchRes = await searchForAlbum(album.name, album.artist.name);
-		const spotifyAlbum = searchRes.find(
-			(a) => a.name.localeCompare(album.name, undefined, { sensitivity: "base" }) === 0,
-		);
-		if (!spotifyAlbum)
-			return {
-				uri: album.url,
-				name: album.name,
-				playcount: Number(album.playcount),
-				type: "lastfm",
-			} as LastFMMinifiedAlbum;
-		return {
-			...minifyAlbum(spotifyAlbum),
-			playcount: Number(album.playcount),
-			name: album.name,
-		} as SpotifyMinifiedAlbum;
+		return searchRes.find((a) => a.name.localeCompare(album.name, undefined, { sensitivity: "base" }) === 0);
 	})({
 		queryKey: ["searchForAlbum", album.name, album.artist.name],
 	});
+	if (!spotifyAlbum)
+		return {
+			uri: album.url,
+			name: album.name,
+			playcount: Number(album.playcount),
+			type: "lastfm",
+		} as LastFMMinifiedAlbum;
+	return {
+		...minifyAlbum(spotifyAlbum),
+		playcount: Number(album.playcount),
+		name: album.name,
+	} as SpotifyMinifiedAlbum;
 };
 
 export const convertTrack = async (track: LastFM.Track) => {
-	return cacher(async () => {
+	const spotifyTrack = await cacher(async () => {
 		const searchRes = await searchForTrack(track.name, track.artist.name);
-		const spotifyTrack = searchRes.find(
-			(t) => t.name.localeCompare(track.name, undefined, { sensitivity: "base" }) === 0,
-		);
-		if (!spotifyTrack)
-			return {
-				uri: track.url,
-				name: track.name,
-				playcount: Number(track.playcount),
-				duration_ms: Number(track.duration) * 1000,
-				artists: [
-					{
-						name: track.artist.name,
-						uri: track.artist.url,
-					},
-				],
-				type: "lastfm",
-			} as LastFMMinifiedTrack;
-		return {
-			...minifyTrack(spotifyTrack),
-			playcount: Number(track.playcount),
-			name: track.name,
-		} as SpotifyMinifiedTrack;
+		return searchRes.find((t) => t.name.localeCompare(track.name, undefined, { sensitivity: "base" }) === 0);
 	})({
 		queryKey: ["searchForTrack", track.name, track.artist.name],
 	});
+	if (!spotifyTrack)
+		return {
+			uri: track.url,
+			name: track.name,
+			playcount: Number(track.playcount),
+			duration_ms: Number(track.duration) * 1000,
+			artists: [
+				{
+					name: track.artist.name,
+					uri: track.artist.url,
+				},
+			],
+			type: "lastfm",
+		} as LastFMMinifiedTrack;
+	return {
+		...minifyTrack(spotifyTrack),
+		playcount: Number(track.playcount),
+		name: track.name,
+	} as SpotifyMinifiedTrack;
 };
