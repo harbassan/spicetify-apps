86a5c0f9d23200ab3a11e6d4beb71ab0ab9f5abb
harbassan <harbassan@hotmail.com>
2024-08-10 18:24:06 +1200

    fix(stats): cache single item for search requests

--- stats/src/utils/converter.ts
+++ stats/src/utils/converter.ts
@@ -49,62 +49,76 @@ export const minifyTrack = (track: Spotify.Track): SpotifyMinifiedTrack => ({
 });
 
 export const convertArtist = async (artist: LastFM.Artist) => {
-	const searchRes = await cacher(() => searchForArtist(artist.name))({ queryKey: ["searchForArtist", artist.name] });
-	const spotifyArtists = searchRes.filter(
-		(a) => a.name.localeCompare(artist.name, undefined, { sensitivity: "base" }) === 0,
-	);
-	const spotifyArtist = spotifyArtists.sort((a, b) => b.popularity - a.popularity)[0];
-	if (!spotifyArtist)
+	return cacher(async () => {
+		const searchRes = await searchForArtist(artist.name);
+		const spotifyArtists = searchRes.filter(
+			(a) => a.name.localeCompare(artist.name, undefined, { sensitivity: "base" }) === 0,
+		);
+		const spotifyArtist = spotifyArtists.sort((a, b) => b.popularity - a.popularity)[0];
+		if (!spotifyArtist)
+			return {
+				name: artist.name,
+				playcount: Number(artist.playcount),
+				uri: artist.url,
+				type: "lastfm",
+			} as LastFMMinifiedArtist;
 		return {
-			name: artist.name,
+			...minifyArtist(spotifyArtist),
 			playcount: Number(artist.playcount),
-			uri: artist.url,
-			type: "lastfm",
-		} as LastFMMinifiedArtist;
-	return {
-		...minifyArtist(spotifyArtist),
-		playcount: Number(artist.playcount),
-		name: artist.name,
-	} as SpotifyMinifiedArtist;
+			name: artist.name,
+		} as SpotifyMinifiedArtist;
+	})({ queryKey: ["searchForArtist", artist.name] });
 };
 
 export const convertAlbum = async (album: LastFM.Album) => {
-	const searchRes = await cacher(() => searchForAlbum(album.name, album.artist.name))({
+	return await cacher(async () => {
+		const searchRes = await searchForAlbum(album.name, album.artist.name);
+		const spotifyAlbum = searchRes.find(
+			(a) => a.name.localeCompare(album.name, undefined, { sensitivity: "base" }) === 0,
+		);
+		if (!spotifyAlbum)
+			return {
+				uri: album.url,
+				name: album.name,
+				playcount: Number(album.playcount),
+				type: "lastfm",
+			} as LastFMMinifiedAlbum;
+		return {
+			...minifyAlbum(spotifyAlbum),
+			playcount: Number(album.playcount),
+			name: album.name,
+		} as SpotifyMinifiedAlbum;
+	})({
 		queryKey: ["searchForAlbum", album.name, album.artist.name],
 	});
-	const spotifyAlbum = searchRes.find(
-		(a) => a.name.localeCompare(album.name, undefined, { sensitivity: "base" }) === 0,
-	);
-	if (!spotifyAlbum)
-		return {
-			uri: album.url,
-			name: album.name,
-			playcount: Number(album.playcount),
-			type: "lastfm",
-		} as LastFMMinifiedAlbum;
-	return { ...minifyAlbum(spotifyAlbum), playcount: Number(album.playcount), name: album.name } as SpotifyMinifiedAlbum;
 };
 
 export const convertTrack = async (track: LastFM.Track) => {
-	const searchRes = await cacher(() => searchForTrack(track.name, track.artist.name))({
+	return cacher(async () => {
+		const searchRes = await searchForTrack(track.name, track.artist.name);
+		const spotifyTrack = searchRes.find(
+			(t) => t.name.localeCompare(track.name, undefined, { sensitivity: "base" }) === 0,
+		);
+		if (!spotifyTrack)
+			return {
+				uri: track.url,
+				name: track.name,
+				playcount: Number(track.playcount),
+				duration_ms: Number(track.duration) * 1000,
+				artists: [
+					{
+						name: track.artist.name,
+						uri: track.artist.url,
+					},
+				],
+				type: "lastfm",
+			} as LastFMMinifiedTrack;
+		return {
+			...minifyTrack(spotifyTrack),
+			playcount: Number(track.playcount),
+			name: track.name,
+		} as SpotifyMinifiedTrack;
+	})({
 		queryKey: ["searchForTrack", track.name, track.artist.name],
 	});
-	const spotifyTrack = searchRes.find(
-		(t) => t.name.localeCompare(track.name, undefined, { sensitivity: "base" }) === 0,
-	);
-	if (!spotifyTrack)
-		return {
-			uri: track.url,
-			name: track.name,
-			playcount: Number(track.playcount),
-			duration_ms: Number(track.duration) * 1000,
-			artists: [
-				{
-					name: track.artist.name,
-					uri: track.artist.url,
-				},
-			],
-			type: "lastfm",
-		} as LastFMMinifiedTrack;
-	return { ...minifyTrack(spotifyTrack), playcount: Number(track.playcount), name: track.name } as SpotifyMinifiedTrack;
 };
