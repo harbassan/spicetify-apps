0aa3b06d25cbc10a97af50d856977dbc8faf46c9
harbassan <harbassan@hotmail.com>
2025-11-30 16:11:59 +1300

    fix(library): search icon

--- library/src/components/searchbar.tsx
+++ library/src/components/searchbar.tsx
@@ -5,12 +5,20 @@ interface SearchBarProps {
 	placeholder?: string;
 }
 
+const searchIconPath = `<path d="M7 1.75a5.25 5.25 0 1 0 0 10.5 5.25 5.25 0 0 0 0-10.5M.25 7a6.75 6.75 0 1 1 12.096 4.12l3.184 3.185a.75.75 0 1 1-1.06 1.06L11.304 12.2A6.75 6.75 0 0 1 .25 7"></path>`;
+
 const SearchBar = (props: SearchBarProps) => {
 	const { setSearch, placeholder } = props;
+	const { IconComponent } = Spicetify.ReactComponent;
 
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setSearch(e.target.value);
 	};
+
+	const searchIcon = <IconComponent className="x-filterBox-searchIcon" size="small" viewBox="0 0 16 16"
+		dangerouslySetInnerHTML={{
+			__html: searchIconPath,
+		}} />
 
 	return (
 		<div className="x-filterBox-filterInputContainer x-filterBox-expandedOrHasFilter" role="search">
@@ -28,27 +36,11 @@ const SearchBar = (props: SearchBarProps) => {
 			/>
 			<div className="x-filterBox-overlay">
 				<span className="x-filterBox-searchIconContainer">
-					<svg
-						data-encore-id="icon"
-						role="img"
-						aria-hidden="true"
-						className="Svg-sc-ytk21e-0 Svg-img-icon-small e-9640-icon x-filterBox-searchIcon"
-						viewBox="0 0 16 16"
-					>
-						<path d="M7 1.75a5.25 5.25 0 1 0 0 10.5 5.25 5.25 0 0 0 0-10.5zM.25 7a6.75 6.75 0 1 1 12.096 4.12l3.184 3.185a.75.75 0 1 1-1.06 1.06L11.304 12.2A6.75 6.75 0 0 1 .25 7z" />
-					</svg>
+					{searchIcon}
 				</span>
 			</div>
 			<button className="x-filterBox-expandButton" aria-hidden="false" aria-label="Search Playlists" type="button">
-				<svg
-					data-encore-id="icon"
-					role="img"
-					aria-hidden="true"
-					className="Svg-sc-ytk21e-0 Svg-img-icon-small x-filterBox-searchIcon"
-					viewBox="0 0 16 16"
-				>
-					<path d="M7 1.75a5.25 5.25 0 1 0 0 10.5 5.25 5.25 0 0 0 0-10.5zM.25 7a6.75 6.75 0 1 1 12.096 4.12l3.184 3.185a.75.75 0 1 1-1.06 1.06L11.304 12.2A6.75 6.75 0 0 1 .25 7z" />
-				</svg>
+				{searchIcon}
 			</button>
 		</div>
 	);
